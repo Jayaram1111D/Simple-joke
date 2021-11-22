ccc4af77e1a0eda01a282cfda92c02da278d5775
unknown <jayarammahesh27@gmail.com>
2021-11-22 20:28:06 +0530

    simple joke

---
+++ script.js
@@ -0,0 +1,26 @@
+//assigning Variables
+
+const jokeEl = document.getElementById('joke');
+
+const jokeBtn = document.getElementById("joke-btn")
+
+jokeBtn.addEventListener('click', generateJoke)
+
+//setInterval(generateJoke,3000)
+
+generateJoke()
+
+async function generateJoke() {
+    const res = await fetch('https://icanhazdadjoke.com', {
+        headers: {
+            Accept: 'application/json'
+       }
+    })
+
+    const data = await res.json()
+
+    console.log(data)
+
+    jokeEl.innerHTML = data.joke
+
+}
